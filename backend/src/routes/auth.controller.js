24e55425e468ab74db4141417a1fdb3d7f5edee0
Shiva7781 <shivavishwakarma7781@gmail.com>
2022-12-04 02:18:46 +0530

    user block for 24 hrs added

--- backend/src/routes/auth.controller.js
+++ backend/src/routes/auth.controller.js
@@ -40,11 +40,60 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(401).json({ error: "User does not exist" });
 
+    // For checking wrong password time of 24 hrs
+    let wrongCountP = user.wrongPasswordCount;
+    let updatedAtT = user.updatedAt;
+
+    let updatedAtDate = new Date(updatedAtT).getDate();
+    // console.log("updatedAtDate:", updatedAtDate);
+
+    let UpdatedAtTime =
+      new Date(updatedAtT).getHours() * 60 + new Date(updatedAtT).getMinutes();
+    // console.log("UpdatedAtTime:", UpdatedAtTime);
+
+    let currentDate = new Date().getDate();
+    // console.log("currentDate:", currentDate);
+
+    let currentTime = new Date().getHours() * 60 + new Date().getMinutes();
+    // console.log("currentTime:", currentTime);
+
+    let DateDiff = Math.floor(updatedAtDate - currentDate);
+    // console.log("DateDiff:", DateDiff);
+    //
+
+    if (
+      (wrongCountP === 5 && DateDiff === 0) ||
+      (wrongCountP === 5 && DateDiff === 1 && currentTime < UpdatedAtTime)
+    ) {
+      return res
+        .status(401)
+        .json({ error: `Your account has been suspended for 24 hours` });
+    }
+
+    // account has been continued after 24 hrs of suspension and setting wrongPasswordCount: 0
+    if (
+      (wrongCountP === 5 && DateDiff > 1) ||
+      (wrongCountP === 5 && DateDiff === 1 && currentTime >= UpdatedAtTime)
+    ) {
+      await User.updateOne(
+        { email: user.email },
+        { $set: { wrongPasswordCount: user.wrongPasswordCount * 0 } }
+      );
+
+      return res.status(401).json({ error: "Please try again" });
+    }
+    //
+
     // checking user password with hashed password stored in the database
     const validPassword = bcrypt.compareSync(req.body.password, user.password);
     console.log(validPassword);
 
     if (!validPassword) {
+      await User.updateOne(
+        { email: user.email },
+        { $set: { wrongPasswordCount: user.wrongPasswordCount + 1 } }
+      );
+
       return res.status(401).json({ error: "Wrong Email/Password" });
     }
 
